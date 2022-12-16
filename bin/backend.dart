06531e5c6dd53e1dd3bb5b9c286f106ab20b5863
Evan Landreneau <evanlandreneau@gmail.com>
2022-12-15 18:35:02 -0600

    Stuff

---
+++ bin/backend.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import "package:backend/backend.dart";
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+void main() async {
+  await db.open();
+  final handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(handleRequest);
+  await shelf_io.serve(handler, "192.168.1.16", 8080);
+
+  print("Running server");
+}
+
+Future<Response> handleRequest(Request requestData) async {
+  final workAppColl = db.collection("work_app");
+  if (requestData.method == "POST") {
+    final incomeData = jsonDecode(await requestData.readAsString());
+    if (incomeData["type"] == "submit") {
+      print(
+          "Somebody by the name of ${incomeData['name']} submitted a new request");
+      final alreadyHasRequests =
+          await workAppColl.findOne({"_id": incomeData["email"]}) != null;
+      if (alreadyHasRequests) {
+        workAppColl.updateOne({
+          "_id": incomeData["email"]
+        }, {
+          "\$push": {"data": incomeData}
+        });
+      } else {
+        workAppColl.insertOne({
+          "_id": incomeData["email"],
+          "data": [incomeData]
+        });
+      }
+
+      return Future.value(Response.ok("Submitted"));
+    } else if (incomeData["type"] == "view") {
+      print("Somebody that used the email ${incomeData['email']} viewed their past requests");
+      {
+        final results =
+            (await workAppColl.find({"_id": incomeData["email"]}).toList())
+                .map((i) => i["data"])
+                .toList();
+        return Future.value(Response.ok(jsonEncode(results)));
+      }
+    } else {
+      return Future.value(Response.ok("")); // Should never get here
+    }
+  } else {
+    return Future.value(Response.ok("")); // Should never get here
+  }
+}
---
+++ lib/backend.dart
@@ -0,0 +1,3 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+final db = Db("mongodb://192.168.1.148");
